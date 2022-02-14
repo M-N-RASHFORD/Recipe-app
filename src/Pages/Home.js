31b89b2e71f5040a9b9c22947514084669ee8879
M-N-RASHFORD <85489724+M-N-RASHFORD@users.noreply.github.com>
2022-02-14 14:26:07 +0300

    Adding pages and components

---
+++ src/Pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+    render = () => {
+        return (
+            <center>
+                <h1>Welcome to ...MR10 Recipe-app</h1>
+                <h2>Recipe app with all;</h2>
+                <ul>
+                    <li>the food products</li>
+                    <li>the ingredients</li>
+                    <li>wines and dish combinations</li>
+                </ul>
+            </center>
+        );
+    }
+}
