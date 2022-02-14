f130f47d559f2e3af5a8a5759c7a748a8d656645
M-N-RASHFORD <85489724+M-N-RASHFORD@users.noreply.github.com>
2022-02-14 16:20:44 +0300

    addinf code to the recipe component

--- src/Pages/Recipes.js
+++ src/Pages/Recipes.js
@@ -2,17 +2,20 @@ import React from 'react';
 import RecipeComponent from '../Components/Recipe';
 
 const Recipes = () => {
+    const [recipes, setRecipes] = React.useState([]);
     const loadData = async () => {
         const resp = await fetch('https://api.spoonacular.com/recipes/random?apiKey=7ad225348b6449479c3055f80e810621&number=1&tags=vegetarian,dessert');
         const data = await resp.json();
-        console.log(data);
+        setRecipes(data.recipes);
+        console.log(recipes);
     }
     React.useEffect(() => {
         loadData();
     }, []);
     return (
         <center>
-            <h1>.....</h1>
+            <h1>...recipes</h1>
+            <RecipeComponent recipes={recipes}/>
         </center>
     );
 }
